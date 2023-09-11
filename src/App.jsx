1713d6362baf0c82ac2a88f0881232f305a70b12
Eray Sahin <eraysahinmoon@gmail.com>
2023-09-11 12:03:40 -0400

    Added Navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Home />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './navbar.css';
+import NetsinerjiLogo from '../../assets/NetsinerjiLogo.png';
+import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa'; // Import hamburger and X icons
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className={`navbar ${isMenuOpen ? 'active' : ''}`}>
+      <div className="left">
+        <Link to="/">
+          <img src={NetsinerjiLogo} alt="netsinerji logo" />
+        </Link>
+        <Link to="/">
+          <h1>Netsinerji</h1>
+        </Link>
+      </div>
+      <div className="right">
+        <div
+          className={`menu-icon ${isMenuOpen ? 'hidden' : ''}`}
+          onClick={toggleMenu}
+        >
+          <FaBars className="bars" color="#fff" />
+        </div>
+        <div
+          className={`menu-icon ${!isMenuOpen ? 'hidden' : ''}`}
+          onClick={toggleMenu}
+        >
+          <FaTimes className="times" color="#fff" />
+        </div>
+        <ul className={`nav-items ${isMenuOpen ? 'active' : ''}`}>
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/">
+            <li>Services</li>
+          </Link>
+          <Link to="/">
+            <li>Info Bank</li>
+          </Link>
+          <Link to="/">
+            <li>About</li>
+          </Link>
+          <Link to="/">
+            <li>Contact</li>
+          </Link>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
