97cf98b90d26ecd45986ddc3e1d7c4d8dacf4925
Eray Sahin <eraysahinmoon@gmail.com>
2023-09-27 17:32:04 -0400

    update pkg json

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,5 @@
 import './home.css';
 import Laptop from '../../assets/Laptop.png';
-import Typist from 'react-typist';
 import Logo from '../../assets/NetsinerjiLogo.png';
 import { Link as ScrollLink } from 'react-scroll';
 
@@ -8,22 +7,12 @@ const Home = () => {
   return (
     <div id="home-section" className="home">
       <div className="home-left">
-        <Typist
-          avgTypingDelay={40}
-          startDelay={500}
-          cursor={{
-            show: true,
-            blink: false,
-            element: '',
-          }}
-        >
-          <div className="img-container">
-            <img src={Logo} alt="Netsinerji Logo" />
-          </div>
-          <h2>
-            Elevate Your <span>Digital Potential</span>.
-          </h2>
-        </Typist>
+        <div className="img-container">
+          <img src={Logo} alt="Netsinerji Logo" />
+        </div>
+        <h2>
+          Elevate Your <span>Digital Potential</span>.
+        </h2>
         <p>
           With <span>expert guidance</span> and cutting-edge solutions, we
           <span> empower businesses and individuals</span> to soar beyond their
